4b6499b5ff5f1017af48da823bfa6ce72e6635c9
Paul-Patrick Nordmann <paul.nordmann@klab-berlin.com>
2015-09-08 18:19:11 +0200

    add a Stream for read dir

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-module.exports = require('./lib');
+var fs = require('fs'),
+	lib = __dirname + '/lib';
+	
+fs.readdirSync(lib).forEach(function(plugin){
+	module.exports[plugin.replace(/\.js$/g, "")] = require(lib + '/' + plugin);
+});
