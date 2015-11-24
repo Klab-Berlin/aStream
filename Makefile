4943124c4e85496b3650b2ebadf18f60ce33aa27
Paul-Patrick Nordmann <paul.nordmann@klab-berlin.com>
2015-11-24 19:34:35 +0100

    fix problems with memory in CreateFileStreams + test

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 test:
-	mocha --reporter spec --require should --ui bdd --timeout 12000 test
+	node_modules/mocha/bin/mocha --reporter spec --require should --ui bdd --timeout 12000 mocha
 
 .PHONY: test
--- mocha/createFileStream.js
+++ mocha/createFileStream.js
@@ -1,40 +1,43 @@
 var rootDir = __dirname.split('/').slice(0, -1).join('/');
 
 var aStream = require(rootDir),
-	should = require('should');
-	
-var dirFiles = [];
+	should = require('should'),
+	fs = require('fs');
+
+var testDir = rootDir + '/lib',
+	dirFiles = [];
 
 before(function(done){
-	new aStream.ReadDir(rootDir + '/lib')
-	.on('data', dirFiles.push.bind(dirFiles))
-	.on('end', function(){
-		if (dirFiles.length === 0) throw new Error('no files found for test in ' + __dirname);
+	fs.readdir(testDir, function(err, data){
+		dirFiles = data.map(function(file){
+			return fs.statSync(testDir + "/" + file).size;
+		})
 		done();
 	})
 })	
 	
 describe("CreateFileStreams", function() {
-	
-	it ('should return buffers and finish', function(done){
+
+	it('should return buffers and finish', function(done){
 		var counter = 0;
-		
-		new aStream.ReadDir(rootDir + '/lib')
+
+		new aStream.ReadDir(testDir)
 		.pipe(new aStream.CreateFileStreams())
 		.on('data', function(data){
 			data.should.be.a.Buffer;
-			counter += 1;
+			dirFiles[0] -= data.length;
+			if (dirFiles[0] === 0) dirFiles.shift();
 		})
 		.pipe(new aStream.WriteNull())
 		.on('finish', function(){
-			counter.should.be.above(dirFiles.length - 1);
+			dirFiles.length.should.equal(0);
 			done()
 		})
-	})
-	
-	it ('should not get something and end by given no files', function(done){
+	});
+
+	it('should not get something and end by given no files', function(done){
 		var counter = 0;
-		
+
 		new aStream.Array([])
 		.pipe(new aStream.CreateFileStreams())
 		.on('data', function(data){
